d36ab4c93f333eb6794bd24a09129a245790f2eb
anveloper <hitedin@gmail.com>
2025-03-29 17:58:13 +0900

    feat: 기본 과제(createVNode, nomalizeVNode) 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,9 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  // 자식은 평탄화 해야한다.
+  children = children.flat(Infinity);
+  // 랜더링 하지 않아야 할 것들을 예외한다.
+  children = children.filter(
+    (c) => c !== false && c !== null && c !== undefined && c !== true,
+  );
+  return { type, props, children };
 }
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,24 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  // console.log(vNode);
+  // 랜더링 제외 / 텍스트 노드 처리
+  const is = (type) => typeof vNode === type;
+  if (vNode === null || vNode === undefined || is("boolean")) return "";
+  else if (is("number") || is("string")) return String(vNode);
+
+  // 컴포넌트 정규화
+  const normalizeChildren = (children) =>
+    (children || []).map(normalizeVNode).filter((c) => c !== "");
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({
+        ...vNode.props,
+        children: normalizeChildren(vNode.children),
+      }),
+    );
+  }
+  return {
+    type: vNode.type,
+    props: vNode.props ?? null,
+    children: normalizeChildren(vNode.children),
+  };
 }
