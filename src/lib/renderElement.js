bf7ce64b2fed1c00b4e1fd47d1b963cfc22481a3
anveloper <hitedin@gmail.com>
2025-03-29 20:04:14 +0900

    refactor: updateElement 파라미터와 변수명 동일하게 변경

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,15 +1,21 @@
 import { createElement } from "./createElement";
 import { setupEventListeners } from "./eventManager";
 import { normalizeVNode } from "./normalizeVNode";
-//import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
 
+let oldNode = null;
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
   if (!vNode) throw new Error();
-  const normalized = normalizeVNode(vNode);
-  const created = createElement(normalized);
-  container.appendChild(created);
+  const newNode = normalizeVNode(vNode);
+  if (!oldNode || !container.firstChild) {
+    const created = createElement(newNode);
+    container.appendChild(created);
+  } else {
+    updateElement(container, newNode, oldNode);
+  }
   setupEventListeners(container);
+  oldNode = newNode;
 }
