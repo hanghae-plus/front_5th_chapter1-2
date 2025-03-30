da70fc37d4795df5c72dbce2e4a1dbef6d5aee42
DohyungKim <ehgud456456@naver.com>
2025-03-30 22:55:13 +0900

    feat: normalizeVNode 를 기반으로 createVNode 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,31 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
+import { normalizeVNode } from "./normalizeVNode";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  const normalizedVNode = normalizeVNode(vNode);
 
-function updateAttributes($el, props) {}
+  if (typeof normalizedVNode === "string") {
+    return document.createTextNode(normalizedVNode);
+  }
+
+  if (Array.isArray(normalizedVNode)) {
+    const el = document.createDocumentFragment();
+    normalizedVNode.forEach((child) => {
+      const childEl = createElement(child);
+      el.appendChild(childEl);
+    });
+    return el;
+  }
+
+  if (typeof normalizedVNode === "object") {
+    const { type, children } = vNode;
+    const el = document.createElement(type);
+    children.forEach((child) => {
+      const childEl = createElement(child);
+      el.appendChild(childEl);
+    });
+    return el;
+  }
+}
+
+// function updateAttributes($el, props) {}
