59f3d87ed6ad4c3ae83397b736e80ac54b1a5be9
maphnew <zcm3118@gmail.com>
2025-03-31 08:10:29 +0900

    Feat: normalizeVNode

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,31 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (
+    vNode === null ||
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean"
+  ) {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode.map(normalizeVNode).filter((node) => node !== "");
+  }
+
+  if (typeof vNode.type === "function") {
+    const component = vNode.type({
+      ...vNode.props,
+      children: vNode.children,
+    });
+    return normalizeVNode(component);
+  }
+
+  return {
+    type: vNode.type,
+    props: vNode.props,
+    children: normalizeVNode(vNode.children),
+  };
 }
