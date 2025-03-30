51b273d9586558fd540605b02fad83754f0cda3a
chaaerim <cofla411@ewhain.net>
2025-03-31 01:23:30 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,4 +1,40 @@
 export function normalizeVNode(vNode) {
+  console.log("normalizeVNode", vNode);
+
+  if (
+    vNode === null ||
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean"
+  ) {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+
+  if (typeof vNode.type === "function") {
+    const propsWithChildren = {
+      ...vNode.props,
+      children: vNode.props?.children || vNode.children,
+    };
+    return normalizeVNode(vNode.type(propsWithChildren));
+  }
+
+  // 일반적인 vNode 객체인 경우 children을 재귀적으로 처리
+  // 위에서 falsy한 값 ""로 return -> 빈 문자열은 filtering 되어야 함
+  if (vNode.children) {
+    console.log("vNode.children", vNode.children);
+    if (Array.isArray(vNode.children)) {
+      vNode.children = vNode.children
+        .map((child) => normalizeVNode(child))
+        .filter((child) => child !== "");
+    } else {
+      const normalizedChild = normalizeVNode(vNode.children);
+      vNode.children = normalizedChild !== "" ? normalizedChild : null;
+    }
+  }
+
   // 최종적으로 변환된 vNode 객체를 반환
   return vNode;
 }
