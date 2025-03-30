93d2282261ac50eb0324a5fa2179e28833749a70
ParkYongHo1 <qkaxhf8823@naver.com>
2025-03-30 23:44:13 +0900

    Feat | createVNode, normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || typeof vNode === "boolean" || vNode === undefined) {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    const component = vNode.type({
+      ...vNode.props,
+      children: vNode.children,
+    });
+    return normalizeVNode(component);
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode.flatMap(normalizeVNode).filter(Boolean);
+  }
+
+  return {
+    ...vNode,
+    children: vNode.children.flatMap(normalizeVNode).filter(Boolean),
+  };
 }
