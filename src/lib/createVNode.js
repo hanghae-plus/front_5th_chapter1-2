4f29840d7fb0163dd52dc95763dae29ef943df85
KumJungMin <rmawjdals@naver.com>
2025-03-30 17:45:02 +0900

    feat: VNode 생성 함수 추가하기

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,23 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  if (isVNode(type)) return type;
+  else {
+    const flattenChildren = getFlattenChildren(children);
+    return { type, props, children: flattenChildren };
+  }
+}
+
+function isVNode(node) {
+  return typeof node === "object" && node !== null && "type" in node;
+}
+
+function getFlattenChildren(children) {
+  if (Array.isArray(children)) {
+    return children.flat(Infinity).filter((child) => !isEmpty(child));
+  } else {
+    return children;
+  }
+}
+
+function isEmpty(value) {
+  return value === false || value === null || value === undefined;
 }
