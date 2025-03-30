8a9a8f5a2cca908ce5cd3bd690b26e03bda7386d
Jeong-wonho <kallzzang@naver.com>
2025-03-31 01:39:28 +0900

    feat normalizeVNode 개발중

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,48 @@
-import { addEvent } from "./eventManager";
+import { normalizeVNode } from "./normalizeVNode.js";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // console.log("vNode's type");
+  // console.log(typeof vNode);
+  // console.log("vNode's content");
+  // console.log(vNode);
+  //테스트 케이스 1 (불리언, undefined, null 처리)
+  if (
+    typeof vNode === "boolean" ||
+    typeof vNode === "undefined" ||
+    vNode === null
+  ) {
+    return document.createTextNode("");
+  }
+  //테스트 케이스 2 (숫자, 스트링 처리)
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
 
-function updateAttributes($el, props) {}
+  //배열일때 처리
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    fragment.append(...vNode.map(createElement));
+    return fragment;
+  }
+
+  /**
+   * 이전에 createElement를 넣는 방식을 확인해봐야할 거 같다.
+   * 어떤형식으로 createElement에 들어가는 걸까?
+   * https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement 예제를 보니 div, span, p 등등
+   * 현재는 type이 [Funtion: TestComponent]임
+   */
+  if (typeof vNode.type === "function") {
+    normalizeVNode(vNode);
+  }
+
+  // FuncitonComponent를 사용했을 때 InvalidCharaterError 발생
+  const el = document.createElement(vNode.type);
+  //함수 일때 처리하기
+  updateAttributes(el, vNode.props);
+  return el;
+}
+
+function updateAttributes($el, props) {
+  console.log("el in updateAttributes", $el);
+  console.log("props", props);
+}
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,35 @@
+// import { createVNode } from "./createVNode.js";
+
 export function normalizeVNode(vNode) {
+  //주어진 가상 노드를 표준화된 형태로 변환하는 역할, 다양한 타입의 입력 처리, 일관된 가상 노드 반환 dom 조작, 렌더링 과정에서 데이터 구조를 활용하는 것이 목표
+  //함수로 넘어오는 데이터를 어떻게 풀어주고 내보낼 수 있을까?
+  // type이 함수인 경우 실행 이미 함수인 것들만 들어왔어.
+  // 이거 두 번 호출되네.
+  console.log("vNode");
+  console.log(vNode);
+  console.log("vNode's type");
+  console.log(vNode.type());
+  const testNode = vNode.type();
+  console.log("testNode");
+  console.log(testNode.type(testNode.children, testNode.props));
+  const testNode2 = testNode.type(testNode.children, testNode.props);
+  console.log("testNode2");
+  console.log(testNode2.props["0"].type(testNode2.children, testNode2.props));
+  /**
+   * 
+   * {
+        type: 'ul', 이게 중요.
+        props: {
+          '0': { type: [Function: ListItem], props: [Object], children: [Array] },
+          '1': { type: [Function: ListItem], props: [Object], children: [Array] },
+          '2': { type: [Function: ListItem], props: [Object], children: [Array] }
+        },
+        children: []
+      } 
+   */
+
+  //type이 function으로 저의 되어 있는데, 무엇을 정의한 것인지 내가 먼저 명확히 알아야 한다.
+
+  // 이걸 만들어야 함수형 컴포넌트를 통과하겠군.
   return vNode;
 }
