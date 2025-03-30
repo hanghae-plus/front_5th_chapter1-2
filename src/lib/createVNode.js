f5ca7892c8ad463ca4b4eaf793957bcd5cbb2137
DohyungKim <ehgud456456@naver.com>
2025-03-30 20:10:59 +0900

    feat: createVNode의 children 중 truthy한 child만 filter

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,10 @@
 export function createVNode(type, props, ...children) {
-  return { type, props, children: children.flat(Infinity) };
+  return {
+    type,
+    props,
+    children: children.flat(Infinity).filter(isTruthy),
+  };
 }
+
+const isFalsy = (value) => [false, null, undefined, ""].includes(value);
+const isTruthy = (value) => !isFalsy(value);
