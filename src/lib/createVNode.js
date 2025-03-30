5d8f32a76bdc178b22a0f1fed5dd12dc1249ccd6
hanbeulYou <hanbeul.you@gmail.com>
2025-03-30 22:20:36 +0900

    fix: basic unit test 통과를 위해 children 필터링 기능 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,7 +1,14 @@
 export function createVNode(type, props, ...children) {
+  const flattenChildren = (arr) => {
+    return arr.reduce((flat, item) => {
+      if (!item && item !== 0) return flat;
+      return flat.concat(Array.isArray(item) ? flattenChildren(item) : item);
+    }, []);
+  };
+
   return {
     type,
     props,
-    children: children.flat(),
+    children: flattenChildren(children),
   };
 }
