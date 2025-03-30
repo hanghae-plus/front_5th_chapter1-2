b1980c5b0fe235eb80934b4c808b68f8baad275c
maphnew <zcm3118@gmail.com>
2025-03-31 08:11:30 +0900

    Feat: createElement, eventManager, renderElement

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,16 @@
-export function setupEventListeners(root) {}
+const eventsMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventsMap.set("root", root);
+  eventsMap.forEach(({ eventType, handler }) => {
+    root.addEventListener(eventType, handler);
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  eventsMap.set(element, { eventType, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  (eventsMap.get("root") || element).removeEventListener(eventType, handler);
+}
--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,8 +3,17 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
+let oldNode = null;
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  let newNode = normalizeVNode(vNode);
+  if (container.childNodes.length === 0) {
+    container.append(createElement(newNode));
+  } else {
+    updateElement(container, newNode, oldNode);
+  }
+  oldNode = newNode;
+  setupEventListeners(container);
 }
