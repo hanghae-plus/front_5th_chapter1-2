2382c61a3ab305276df024dd0f53cd8c765e6090
상은 <1-blue98@naver.com>
2025-03-30 13:25:11 +0900

    feat: eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,47 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+/**
+ * @param {HTMLElement} root
+ */
+export function setupEventListeners(root) {
+  const exRoot = eventMap.get("root");
+  if (exRoot !== root) {
+    eventMap.set("root", root);
+  }
 
-export function removeEvent(element, eventType, handler) {}
+  eventMap.forEach((event) => {
+    Object.entries(event)
+      .filter(([, handler]) => typeof handler === "function")
+      .forEach(([eventType, handler]) => {
+        root.addEventListener(eventType, handler);
+      });
+  });
+}
+
+/**
+ * @param {HTMLElement} element
+ * @param {keyof WindowEventMap} eventType
+ * @param {Function} handler
+ */
+export function addEvent(element, eventType, handler) {
+  eventMap.set(element, {
+    ...eventMap.get(element),
+    [eventType]: handler,
+  });
+}
+
+/**
+ * @param {HTMLElement} element
+ * @param {keyof WindowEventMap} eventType
+ * @param {Function} handler
+ */
+export function removeEvent(element, eventType, handler) {
+  const exEvent = eventMap.get(element);
+  const hasEvent = !!exEvent;
+  if (hasEvent) {
+    eventMap.delete(element);
+  }
+
+  const root = eventMap.get("root");
+  root.removeEventListener(eventType, handler);
+}
