21bf1e6690f67905cf2aa8fe957451b7b82002b1
상은 <1-blue98@naver.com>
2025-03-30 11:46:43 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,19 @@
+/**
+ * @param {keyof HTMLElementTagNameMap} type - 노드의 영문명
+ * @param {Object} props - 노드의 속성
+ * @param {...any} children - 노드의 자식
+ * @returns {{
+ *  type: keyof HTMLElementTagNameMap,
+ *  props: Object,
+ *  children: any[]
+ * }} VNode
+ */
 export function createVNode(type, props, ...children) {
-  return {};
+  const flattenChildren = children.flat(Infinity);
+
+  return {
+    type,
+    props,
+    children: flattenChildren,
+  };
 }
