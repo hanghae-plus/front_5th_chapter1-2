67a9f021deba0d7d840c9511b3f5c517b1c4d1a0
hsw824 <ghdtjddn0824@naver.com>
2025-03-31 00:49:01 +0900

    feat: createVNode 함수 완성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,13 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  const flatChildren = children.flat(Infinity);
+  const renderChildren = flatChildren.filter(
+    (item) => item !== null && item !== undefined && item !== false,
+  );
+
+  const propType = !props ? null : props;
+  return {
+    type,
+    props: propType,
+    children: renderChildren,
+  };
 }
