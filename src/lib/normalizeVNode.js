6ee261699db8dc39fa0dba4d44276b9021ce627b
DohyungKim <ehgud456456@naver.com>
2025-03-30 22:29:04 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,31 @@
 export function normalizeVNode(vNode) {
+  if (typeof vNode === "string") {
+    return vNode;
+  }
+
+  if (typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (vNode == null || typeof vNode === "boolean" || vNode === undefined) {
+    return "";
+  }
+
+  if (typeof vNode.type === "function") {
+    const functionNode = vNode.type({
+      children: vNode.children,
+      ...vNode.props,
+    });
+
+    functionNode.props = {
+      ...(vNode.props || {}),
+      ...(functionNode.props || {}),
+    };
+
+    functionNode.children = functionNode.children.map(normalizeVNode);
+
+    return normalizeVNode(functionNode);
+  }
+
   return vNode;
 }
