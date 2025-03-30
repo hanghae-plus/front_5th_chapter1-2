42df9dbb8eaad717bcb2482a39c19203836c5c01
hsw824 <ghdtjddn0824@naver.com>
2025-03-31 00:49:21 +0900

    feat: normalizeVNode 함수 완성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean" ||
+    vNode === null
+  ) {
+    return "";
+  }
+
+  if (typeof vNode === "object") {
+    const { type, props, children } = vNode;
+
+    if (typeof type === "function") {
+      return normalizeVNode(type({ ...props, children }));
+    }
+    return {
+      type,
+      props,
+      children: children
+        .map((child) => normalizeVNode(child))
+        .filter((normalizeChild) => normalizeChild !== ""),
+    };
+  }
+
+  return String(vNode);
 }
