85363787cf476586f66dfd7d2250619efeb3794e
hsw824 <ghdtjddn0824@naver.com>
2025-03-31 23:58:03 +0900

    feat: eventManager 함수 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,30 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+let rootElement = null;
 
-export function removeEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  if (!root) return;
+  rootElement = root;
+  eventMap.forEach((handlers, eventType) => {
+    handlers.forEach((handler) => {
+      rootElement.addEventListener(eventType, handler);
+    });
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventMap.has(eventType)) {
+    eventMap.set(eventType, new Map());
+  }
+
+  const handlers = eventMap.get(eventType);
+  handlers.set(element, handler);
+}
+
+export function removeEvent(element, eventType, handler) {
+  const handlers = eventMap.get(eventType);
+  if (handlers.get(element)) {
+    handlers.delete(element);
+    rootElement.removeEventListener(eventType, handler);
+  }
+}
