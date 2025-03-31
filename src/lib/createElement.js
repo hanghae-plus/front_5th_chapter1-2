8653d64567ff8c6d1714a066af3e7ff1bab656ae
Heejin <dev.heejiin@gmail.com>
2025-04-01 00:24:59 +0900

    feat: basic test 추가 (이벤트, 렌더링 제외)

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,43 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === undefined || vNode === null || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    vNode.forEach((child) => {
+      const childElement = createElement(child);
+      if (childElement) {
+        fragment.appendChild(childElement);
+      }
+    });
+
+    return fragment;
+  }
+
+  if (typeof vNode === "object" && vNode.type) {
+    const element = document.createElement(vNode.type);
+    Object.entries(vNode.props || {})
+      .filter(([, value]) => value)
+      .forEach(([attribute, value]) => {
+        if (attribute === "className") {
+          element.setAttribute("class", value);
+        } else {
+          element.setAttribute(attribute, value);
+        }
+      });
+
+    const children = vNode.children?.map(createElement) || [];
+    children.forEach((child) => element.appendChild(child));
+    return element;
+  }
+}
 
 function updateAttributes($el, props) {}
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,47 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return `${vNode}`;
+  }
+
+  if (typeof vNode === "object") {
+    if (typeof vNode.type === "function") {
+      const Component = vNode.type;
+      const props = { ...vNode.props };
+
+      if (vNode.children && Array.isArray(vNode.children)) {
+        props.children = vNode.children;
+      }
+
+      const result = Component(props);
+
+      return normalizeVNode(result);
+    }
+
+    return {
+      type: vNode.type,
+      props: vNode.props ?? null,
+      children: Array.isArray(vNode.children)
+        ? vNode.children
+            .flat(Infinity)
+            .filter(
+              (child) =>
+                child !== null &&
+                child !== undefined &&
+                typeof child !== "boolean",
+            )
+            .map((child) => normalizeVNode(child))
+        : [],
+    };
+  }
+
+  if (typeof vNode === "function") {
+    const result = vNode();
+    return normalizeVNode(result);
+  }
+
   return vNode;
 }
