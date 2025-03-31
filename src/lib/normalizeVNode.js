8802737710fd397b603d0dafc54d2b4210acebe3
minhee <jmh3699@naver.com>
2025-03-31 12:34:34 +0900

    feat: normalizeVNode 인자 컴포넌트 정규화 및 테스트 코드
    인자의 type이 함수라면 children과 props를 함께 재귀해 정규화를 한다.

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,38 @@
+const isEmptyStrType = (val) => {
+  return val === null || typeof val === "undefined" || typeof val === "boolean";
+};
+
+const isNumericOrStr = (val) => {
+  return !isNaN(Number(val)) || typeof val === "string";
+};
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  // null, undefined, boolean 값은 빈 문자열로 변환
+  if (isEmptyStrType(vNode)) return "";
+
+  // 문자열과 숫자는 문자열로 변환
+  if (isNumericOrStr(vNode)) return String(vNode);
+
+  // 컴포넌트 정규화
+  const { type, props, children } = vNode;
+
+  if (!type && !props && !children) {
+    return;
+  }
+
+  if (typeof type === "function") {
+    return normalizeVNode(type({ children, ...(props || {}) }));
+  }
+
+  const normalizedChildren = Array.isArray(children)
+    ? children.map(normalizeVNode)
+    : children
+      ? [normalizeVNode(children)]
+      : [];
+
+  return {
+    type,
+    props: props ?? null,
+    children: normalizedChildren,
+  };
 }
