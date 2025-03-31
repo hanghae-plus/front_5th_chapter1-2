51e5dbd0c3303c740fa8978c4df25a033f9b4af6
KumJungMin <rmawjdals@naver.com>
2025-04-01 00:11:59 +0900

    chore: base 주소에 따라 배포 설정하기

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,18 @@
 import { createObserver } from "./createObserver";
 
 export const createRouter = (routes) => {
+  const basePath = import.meta.env.VITE_BASE_PATH ?? "/";
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const formatPath = (path) => path.replace(basePath, "/");
+
+  const getPath = () => formatPath(window.location.pathname);
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
-    window.history.pushState(null, null, path);
+    const newPath = formatPath(path);
+    window.history.pushState(null, null, newPath);
     notify();
   };
 
