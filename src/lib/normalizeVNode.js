f59e9d4034209e3eb24b7ce6e293c92290100ca1
dh-baselabs <donghyun@baselabs.co.kr>
2025-04-01 01:26:32 +1100

    feat: normalizeVNode 정규화 완료

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode == null || typeof vNode === "boolean") {
+    return "";
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+  if (typeof vNode.type === "function") {
+    const node = vNode.type({ ...vNode.props, children: vNode.children });
+    return normalizeVNode(node);
+  } else {
+    return {
+      ...vNode,
+      children: vNode.children
+        .flatMap(normalizeVNode)
+        .filter((item) => Boolean(item)),
+    };
+  }
 }
