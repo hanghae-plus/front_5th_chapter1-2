91707938e5e999e3736c337e244015c7303681a5
JiYu <eunjun1018@gmail.com>
2025-04-01 01:07:52 +0900

    normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (
+    vNode === null ||
+    vNode === undefined ||
+    vNode === true ||
+    vNode === false
+  ) {
+    vNode = "";
+    return vNode;
+  }
+
+  // typeof는 항상 문자열을 반환한다.
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+  // if(typeof vNode === "number") {
+  //   return String(vNode);
+  // }
 }
