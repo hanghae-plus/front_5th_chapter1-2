9e04dac7682f5a427923f3883320449290dc69ad
dh-baselabs <donghyun@baselabs.co.kr>
2025-04-01 02:32:20 +1100

    feat: createElement 진짜 DOM 만들기

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,39 @@
-import { addEvent } from "./eventManager";
+export function createElement(vNode) {
+  if (vNode == null || typeof vNode === "boolean")
+    return document.createTextNode("");
+  if (typeof vNode === "string" || typeof vNode === "number")
+    return document.createTextNode(String(vNode));
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((e) => fragment.appendChild(createElement(e)));
+    return fragment;
+  }
+  if (typeof vNode.type === "string") {
+    const el = document.createElement(vNode.type);
+    const props = vNode.props || {};
+    updateAttributes(el, props);
+    []
+      .concat(vNode.children || [])
+      .forEach((c) => el.appendChild(createElement(c)));
+    return el;
+  }
+  throw new Error();
+}
 
-export function createElement(vNode) {}
+function updateAttributes($el, props = {}) {
+  for (const [key, value] of Object.entries(props)) {
+    if (key === "children") continue;
 
-function updateAttributes($el, props) {}
+    const attr = key === "className" ? "class" : key;
+
+    if (typeof value === "boolean") {
+      if (value) {
+        $el.setAttribute(attr, "");
+      } else {
+        $el.removeAttribute(attr);
+      }
+    } else {
+      $el.setAttribute(attr, value);
+    }
+  }
+}
