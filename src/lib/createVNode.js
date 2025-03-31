f9f2fe06e110e1b9cd8794bfd2f8b08de14f8f96
YUJIN <yujin@YUJINui-MacBookAir.local>
2025-04-01 01:27:20 +0900

    fix: createVNode.js 테스트 통과

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,13 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children.flat(Infinity).filter((v) => v === 0 || Boolean(v)),
+    // 평탄화
+    // .flat(Infinity)
+
+    // 조건부 렌더링
+    // .filter(Boolean) : truthy한 값 반환
+    // .filter((v) => v === 0 || Boolean(v)) : 0 포함
+  };
 }
