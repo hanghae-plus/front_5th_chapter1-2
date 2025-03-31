f2e2f061ec6f552656f47a7a355bdfb53b63d878
3jun <kk3june@gmail.com>
2025-04-01 01:48:47 +0900

    feat. createElement 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,41 @@
-import { addEvent } from "./eventManager";
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode.toString());
+  }
 
-export function createElement(vNode) {}
+  if (vNode.length) {
+    const $fragment = document.createDocumentFragment();
+    vNode.forEach((it) => {
+      const tag = document.createElement(it.type);
+      tag.textContent = it.children.flat(Infinity);
+      $fragment.appendChild(tag);
+    });
+    return $fragment;
+  }
 
-function updateAttributes($el, props) {}
+  const $el = document.createElement(vNode.type);
+  updateAttributes($el, vNode.props);
+
+  if (vNode.children) {
+    vNode.children.forEach((child) => {
+      const childNode = createElement(child);
+      $el.appendChild(childNode);
+    });
+  }
+
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  if (!props) return;
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.setAttribute("class", value);
+      return;
+    }
+    $el.setAttribute(key, value);
+  });
+}
