a4e9183d3904766e95ddc80e5b39578b11de7568
Doh Kim <ehgud456456@naver.com>
2025-03-31 18:00:14 +0900

    fix: root에 이벤트 중복 등록 방지

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,15 +1,32 @@
+// root에 등록해둔 이벤트 타입을 Set+Map으로 관리
+const rootMap = new Map();
 const eventTypeMap = new Map();
 
 export function setupEventListeners(root) {
-  for (const [eventType, typeEvents] of eventTypeMap) {
+  if (!rootMap.has(root)) {
+    rootMap.set(root, new Set());
+  }
+
+  // root에 연결되어 있는 이벤트 타입들
+  const pastEvents = rootMap.get(root);
+
+  eventTypeMap.forEach((typeEvents, eventType) => {
+    // 이벤트 타입이 root에 등록되어 있다면 리턴
+    if (pastEvents.has(eventType)) {
+      return;
+    }
+
+    // 이벤트 타입이 root에 등록되어 있지 않다면 이벤트 리스너 등록
     root.addEventListener(eventType, (event) => {
       const target = event.target;
+      const typeEvents = eventTypeMap.get(eventType);
       if (typeEvents.has(target)) {
         const handler = typeEvents.get(target);
         handler(event);
       }
     });
-  }
+    pastEvents.add(eventType);
+  });
 }
 
 export function addEvent(element, eventType, handler) {
