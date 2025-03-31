7b70f7ee3b71627912278fa38b6ce174274e291c
minhee <jmh3699@naver.com>
2025-03-31 21:22:22 +0900

    feat: createElement 인자가 컴포넌트일 때 구현
    `createElement`, `setAttribute`, `createTextNode`, `appendChild` 등의 API를 이용해 재귀적으로 컴포넌트를 element로 변환한다.

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -16,4 +16,32 @@ export function createElement(vNode) {
       childNodes: childNodes,
     };
   }
+
+  if (typeof vNode.type === "function") {
+    throw new Error("함수 컴포넌트는 createElement로 처리할 수 없습니다.");
+  }
+
+  // 컴포넌트를 element로 만들어 반환하기
+  const { type, props, children } = vNode;
+  const element = document.createElement(type);
+
+  if (props) {
+    Object.keys(props).forEach((key) => {
+      const attributeName = key === "className" ? "class" : key;
+      element.setAttribute(attributeName, props[key]);
+    });
+  }
+
+  if (children && Array.isArray(children)) {
+    children.forEach((child) => {
+      const childElement = createElement(child);
+      if (childElement.nodeType === Node.TEXT_NODE) {
+        element.appendChild(document.createTextNode(childElement.textContent));
+      } else {
+        element.appendChild(childElement);
+      }
+    });
+  }
+
+  return element;
 }
