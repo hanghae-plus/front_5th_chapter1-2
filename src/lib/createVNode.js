d45d9d6410a0801e1e054da7ecb2c19cba8622e3
choiseoeun-GoUp <seoeun0321@gmail.com>
2025-03-31 23:21:34 +0900

    Revert "feat : renderElement 제외 Basic Test"
    This reverts commit 67513500c11af02e7ef22ab5861394caced18b63.

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,9 +1,3 @@
 export function createVNode(type, props, ...children) {
-  // 자식 배열을 평탄화하고 null, undefined, false 등은 제외
-  const flattenedChildren = children
-    .flat(Infinity)
-    .filter((item) => item !== null && item !== undefined && item !== false);
-  // flat(Infinity)는 중첩된 배열이 몇 단계가 있든지 모든 배열을 평탄화함
-
-  return { type, props, children: flattenedChildren };
+  return {};
 }
--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -4,20 +4,7 @@ import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  let originNode;
-  const normalizedVNode = normalizeVNode(vNode);
-
-  const element = createElement(normalizedVNode);
-
-  if (container.children.length === 0) {
-    // 최초 렌더링시에는 createElement로 DOM을 생성하고
-    container.appendChild(element);
-    originNode = vNode;
-  } else {
-    // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-    updateElement(container, vNode, originNode);
-  }
-
+  // 최초 렌더링시에는 createElement로 DOM을 생성하고
+  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
-  setupEventListeners(container);
 }
