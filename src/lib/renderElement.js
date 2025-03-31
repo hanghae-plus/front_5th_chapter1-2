67513500c11af02e7ef22ab5861394caced18b63
ui-choiseoeun <79019104+choiseoeun-GoUp@users.noreply.github.com>
2025-03-31 23:19:04 +0900

    feat : renderElement 제외 Basic Test

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -4,7 +4,20 @@ import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+  let originNode;
+  const normalizedVNode = normalizeVNode(vNode);
+
+  const element = createElement(normalizedVNode);
+
+  if (container.children.length === 0) {
+    // 최초 렌더링시에는 createElement로 DOM을 생성하고
+    container.appendChild(element);
+    originNode = vNode;
+  } else {
+    // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+    updateElement(container, vNode, originNode);
+  }
+
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  setupEventListeners(container);
 }
