b0a5d3c3e87a7ee2b4cec529091967278974a428
상은 <1-blue98@naver.com>
2025-03-31 21:53:58 +0900

    refactor: advanced.test.js 통과

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,7 +1,10 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
+
+// https://ko.javascript.info/weakmap-weakset
+const elementMap = new Map();
 
 /**
  * @param {VNode} vNode
@@ -11,20 +14,20 @@ export function renderElement(vNode, container) {
   // 정규화
   const normalizedVNode = normalizeVNode(vNode);
 
-  // 노드 생성
-  const element = createElement(normalizedVNode);
-
   const isFirstRender = container.children.length === 0;
 
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   if (isFirstRender) {
-    container.appendChild(element);
+    const newElement = createElement(normalizedVNode);
+    container.appendChild(newElement);
   }
-  // FIXME: 이후에는 updateElement로 기존 DOM을 업데이트한다.
+  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   else {
-    container.innerHTML = "";
-    container.appendChild(element);
+    const oldVNode = elementMap.get("vNode");
+    updateElement(container, normalizedVNode, oldVNode);
   }
+
+  elementMap.set("vNode", normalizedVNode);
 
   // 이벤트 등록
   setupEventListeners(container);
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,98 @@
-import { addEvent, removeEvent } from "./eventManager";
+// import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+/**
+ * @param {HTMLElement} target
+ * @param {Object} newProps
+ * @param {Object} oldProps
+ */
+function updateAttributes(target, newProps, oldProps) {
+  // 다른거 갈아끼우고 + 생긴거 추가하고
+  Object.entries(newProps).map(([key, value]) => {
+    if (oldProps[key] === value) return;
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+    if (key === "className") key = "class";
+    if (key === "htmlFor") key = "for";
+
+    target.setAttribute(key, value);
+  });
+
+  // 없어진거 제거
+  Object.keys(oldProps).map((key) => {
+    if (newProps[key] !== undefined) return;
+
+    target.removeAttribute(key);
+  });
+}
+
+/**
+ * 가상 DOM 노드 타입 정의
+ * @typedef {Object} VNode
+ * @property {keyof HTMLElementTagNameMap|Function} type - 노드의 타입 ( HTML 태그명 or 컴포넌트 함수 )
+ * @property {Object|null} props - 노드의 속성들
+ * @property {Array<string|number|VNode>} children - 자식 노드들
+ */
+
+/**
+ * 가상노드를 비교 후 실제 노드로 변환해서 업데이트하는 함수
+ * @link https://junilhwang.github.io/TIL/Javascript/Design/Vanilla-JS-Virtual-DOM/#_4-diff-%E1%84%8B%E1%85%A1%E1%86%AF%E1%84%80%E1%85%A9%E1%84%85%E1%85%B5%E1%84%8C%E1%85%B3%E1%86%B7-%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%AD%E1%86%BC
+ *
+ * @param {HTMLElement} parentElement 실제 Element
+ * @param {VNode | string} newVNode 새로운 가상 노드
+ * @param {VNode | string} oldVNode 기존 가상 노드
+ * @param {number} index 노드의 깊이
+ */
+export function updateElement(parentElement, newVNode, oldVNode, index = 0) {
+  // 1. 노드 제거 ( newVNode가 없고 oldVNode가 있는 경우 )
+  if (!newVNode && oldVNode) {
+    return parentElement.removeChild(parentElement.childNodes[index]);
+  }
+
+  // 2. 노드 추가 ( newVNode가 있고 oldVNode가 없는 경우 )
+  if (newVNode && !oldVNode) {
+    return parentElement.appendChild(createElement(newVNode));
+  }
+
+  // 3. 텍스트 노드 ( newVNode와 oldVNode가 모두 텍스트 노드인 경우 )
+  if (typeof newVNode === "string" && typeof oldVNode === "string") {
+    if (newVNode === oldVNode) return;
+
+    return parentElement.replaceChild(
+      createElement(newVNode),
+      parentElement.childNodes[index],
+    );
+  }
+
+  // 4. 노드 교체 ( newVNode와 oldVNode의 타입이 다른 경우 )
+  if (newVNode.type !== oldVNode.type) {
+    return parentElement.replaceChild(
+      createElement(newVNode),
+      parentElement.childNodes[index],
+    );
+  }
+
+  // 5. 노드 업데이트 ( newVNode와 oldVNode의 타입이 같은데 속성이 다른 경우 )
+  if (newVNode.type === oldVNode.type) {
+    updateAttributes(
+      parentElement.childNodes[index],
+      newVNode.props ?? {},
+      oldVNode.props ?? {},
+    );
+  }
+
+  // 재귀 ( 모든 children의 children의 ... 돌고 탈출 )
+  const maxLength = Math.max(
+    newVNode.children?.length ?? 0,
+    oldVNode.children?.length ?? 0,
+  );
+  Array(maxLength)
+    .fill(null)
+    .forEach((_, i) =>
+      updateElement(
+        parentElement.childNodes[index],
+        newVNode.children[i],
+        oldVNode.children[i],
+        i,
+      ),
+    );
+}
