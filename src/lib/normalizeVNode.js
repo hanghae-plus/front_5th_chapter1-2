ed3111bdc42104dd337710135b80ffd14df9a6a3
User850413 <csyou1228@gmail.com>
2025-04-01 22:22:13 +0900

    04/01 :necktie: : normalizeVNode 타입 검사 및 컴포넌트 정규화

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,30 @@
 export function normalizeVNode(vNode) {
+  if (
+    vNode === null ||
+    vNode === undefined ||
+    vNode === false ||
+    vNode === true
+  ) {
+    return "";
+  } else if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+
+  // 정규화
+  if (typeof vNode.type === "function") {
+    const nodeFn = vNode.type({
+      children: vNode.children,
+      ...vNode.props,
+    });
+
+    nodeFn.props = {
+      ...(vNode.props || {}),
+      ...(nodeFn.props || {}),
+    };
+
+    nodeFn.children = nodeFn.children.map((node) => normalizeVNode(node));
+    return normalizeVNode(nodeFn);
+  }
+
   return vNode;
 }
