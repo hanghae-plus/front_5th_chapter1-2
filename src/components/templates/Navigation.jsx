e8c7fda483237db745bebc0aab8e1be4faccc5f8
hanbeulYou <hanbeul.you@gmail.com>
2025-04-02 00:13:13 +0900

    feat: Github 배포용 코드 추가

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -3,9 +3,14 @@ import { createVNode } from "../../lib";
 import { router } from "../../router";
 import { globalStore } from "../../stores";
 
+const BASE_PATH =
+  process.env.NODE_ENV === "production" ? "/front_5th_chapter1-2" : "";
+
 const getNavItemClass = (path) => {
   const currentPath = window.location.pathname;
-  return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
+  return currentPath === BASE_PATH + path
+    ? "text-blue-600 font-bold"
+    : "text-gray-600";
 };
 
 function Link({ onClick, children, ...props }) {
