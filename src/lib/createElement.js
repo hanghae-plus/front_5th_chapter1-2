399617edaf403a6cbdca3e2d63a273d3633ea796
hyunjin <71summernight@naver.com>
2025-04-02 01:14:41 +0900

    feat: createVNode, normalizeVNode 함수 작성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,20 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
 
-function updateAttributes($el, props) {}
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.flat().forEach((child) => {
+      const el = createElement(child);
+      fragment.appendChild(el);
+    });
+    return fragment;
+  }
+}
+// function updateAttributes($el, props) {}
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,21 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return "";
+  if (typeof vNode === "string" || typeof vNode === "number")
+    return vNode.toString();
+  if (Object.prototype.toString.call(vNode) === "[object Function]")
+    return normalizeVNode(vNode());
+  if (Array.isArray(vNode))
+    return vNode
+      .map(normalizeVNode)
+      .filter((child) => typeof child === "string")
+      .join("");
+  if (Object.prototype.toString.call(vNode) === "[object Object]") {
+    const normalized = { ...vNode };
+    if (normalized.children) {
+      normalized.children = normalizeVNode(normalized.children);
+    }
+    return normalized;
+  }
+  return "";
 }
