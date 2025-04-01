cadd0eeec6f9a80be5a160a482a99541e223a2f3
naroso_o <naroso1751@naver.com>
2025-04-02 08:15:36 +0900

    feat: normalizeVNode

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,46 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // 빈 문자열
+  if (
+    vNode === null ||
+    vNode === undefined ||
+    vNode === true ||
+    vNode === false
+  ) {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  // 단순 문자열과 숫자
+  if (typeof vNode == "string" || typeof vNode == "number") {
+    return document.createTextNode(vNode);
+  }
+
+  // 배열
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      const el = createElement(child);
+      fragment.appendChild(el);
+    });
+    return fragment;
+  }
+
+  // 오브젝트
+  if (typeof vNode == "object") {
+    const el = document.createElement(vNode.type);
+    const props = vNode.props ? vNode.props : {};
+    const children = vNode.children;
+
+    if (props && props.length) {
+      props.forEach((prop) => el.setAttribute(prop.key, prop.value));
+    }
+
+    if (children && children.length) {
+      children.forEach((child) => el.appendChild(createElement(child)));
+    }
+
+    return el;
+  }
+}
+// function updateAttributes($el, props) {}
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,42 @@
+const isValidateNode = (node) => {
+  if (node === null || node === undefined || node === true || node === false) {
+    return false;
+  }
+  return true;
+};
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  // 빈 문자열
+  if (!isValidateNode(vNode)) {
+    return "";
+  }
+
+  // 단순 문자열과 숫자
+  if (typeof vNode == "string" || typeof vNode == "number") {
+    return `${vNode}`;
+  }
+
+  // 오브젝트
+  if (typeof vNode == "object") {
+    const { type, props = {}, children = [] } = vNode;
+    // 1. type: function
+    if (typeof vNode.type == "function") {
+      // 함수형 컴포넌트를 사용하듯이, props와 children 추가
+      return normalizeVNode(vNode.type({ ...props, children }));
+    }
+
+    // 2. type: string
+    if (typeof vNode.type == "string") {
+      const normalizedChildren = [];
+      if (Array.isArray(children)) {
+        children.forEach(
+          (child) =>
+            isValidateNode(child) &&
+            normalizedChildren.push(normalizeVNode(child)),
+        );
+      }
+      // Object 타입의 VNode 반환
+      return { type, props, children: normalizedChildren };
+    }
+  }
 }
