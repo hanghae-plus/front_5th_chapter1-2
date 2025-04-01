707d10d6b4b30398dbc5d71af270dbeccb857567
Doh Kim <ehgud456456@naver.com>
2025-04-01 12:08:37 +0900

    refactor: 변수명, 주석 강화

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,31 +1,33 @@
-// root에 등록해둔 이벤트 타입을 Set+Map으로 관리
-const rootMap = new Map();
+// 등록해둔 이벤트 타입을 Map[root, Set[eventType]] 으로 관리
+const rootEventMap = new Map();
 const eventTypeMap = new Map();
 
 export function setupEventListeners(root) {
-  if (!rootMap.has(root)) {
-    rootMap.set(root, new Set());
+  // root에 등록할 eventType 집합 초기화
+  if (!rootEventMap.has(root)) {
+    rootEventMap.set(root, new Set());
   }
 
   // root에 연결되어 있는 이벤트 타입들
-  const pastEvents = rootMap.get(root);
+  const rootEvents = rootEventMap.get(root);
 
   eventTypeMap.forEach((typeEvents, eventType) => {
     // 이벤트 타입이 root에 등록되어 있다면 리턴
-    if (pastEvents.has(eventType)) {
-      return;
-    }
+    if (rootEvents.has(eventType)) return;
+
+    // 이벤트 타입을 root에 등록
+    rootEvents.add(eventType);
 
     // 이벤트 타입이 root에 등록되어 있지 않다면 이벤트 리스너 등록
+    // 'typeEvents를 순회해서, 실행해야 하는 핸들러 있으면 실행한다' 라는 이벤트를 부착
     root.addEventListener(eventType, (event) => {
       const target = event.target;
-      // 이벤트가 발생한 target의 handler가 Map에 있다면
+      // 이벤트가 발생한 target의 handler가 Map에 있다면 수행
       if (typeEvents.has(target)) {
         const handler = typeEvents.get(target);
         handler(event);
       }
     });
-    pastEvents.add(eventType);
   });
 }
 
