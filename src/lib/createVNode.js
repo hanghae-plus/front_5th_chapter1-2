21b6276c8ca4f683604852107fd7a29145edbdfe
dlsxody1 <dlsxody1@naver.com>
2025-04-01 13:53:42 +0900

    chore : createVnode 재구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,26 +1,9 @@
 export function createVNode(type, props, ...children) {
-  // children 평탄화 (재귀적으로 중첩된 배열도 처리)
-  const flattenChildren = (items) => {
-    return items.reduce((acc, item) => {
-      // null, undefined, true, false 같은 값 제외 조건부처리
-      if (
-        item === null ||
-        item === undefined ||
-        item === true ||
-        item === false
-      ) {
-        return acc;
-      }
-      if (Array.isArray(item)) {
-        return [...acc, ...flattenChildren(item)];
-      }
-      return [...acc, item];
-    }, []);
-  };
-
   return {
     type,
-    props: props || null,
-    children: flattenChildren(children),
+    props,
+    children: children.flat(Infinity).filter((el) => {
+      return el !== null && el !== undefined && el !== false && el !== true;
+    }),
   };
 }
