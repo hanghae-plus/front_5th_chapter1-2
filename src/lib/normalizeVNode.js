8fb939340c3e523857d49b8b52f5b566940fd0e3
JiYu <eunjun1018@gmail.com>
2025-04-01 15:03:56 +0900

    normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,5 @@
+// import { createVNode } from "./createVNode";
+
 export function normalizeVNode(vNode) {
   if (
     vNode === null ||
@@ -13,7 +15,28 @@ export function normalizeVNode(vNode) {
   if (typeof vNode === "string" || typeof vNode === "number") {
     return String(vNode);
   }
-  // if(typeof vNode === "number") {
-  //   return String(vNode);
-  // }
+
+  // console.log(vNode);
+  // console.log(vNode.type);
+
+  if (typeof vNode.type === "function") {
+    // console.log('testing');
+    // return createVNode(vNode.type, vNode.type.props, vNode.children);
+
+    // 함수형 컴포넌트가 반환하는 객체 처리
+    const element = vNode.type({
+      ...vNode.props, // props 전달
+      children: vNode.children, // chidren 전달
+    });
+
+    return normalizeVNode(element);
+  }
+
+  return {
+    type: vNode.type,
+    props: vNode.props,
+    children: vNode.children
+      .map(normalizeVNode)
+      .filter((child) => child !== ""),
+  };
 }
