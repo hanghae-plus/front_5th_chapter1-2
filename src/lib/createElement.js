225eb83ada77fabd9a18cba1ac5df979f6594619
DESKTOP-3BCHKHG\user <jonjour99@gmail.com>
2025-04-01 23:04:12 +0900

    :ambulance: 가상 DOM 만들기 START

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,28 @@
-import { addEvent } from "./eventManager";
+//import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (
+    typeof vNode === "boolean" ||
+    typeof vNode === "undefined" ||
+    vNode === null
+  ) {
+    return document.createTextNode("");
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
 
-function updateAttributes($el, props) {}
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    fragment.append(...vNode.map(createElement));
+    return fragment;
+  }
+
+  const el = document.createElement(vNode.type);
+  updateAttributes(el, vNode.props);
+  return el;
+}
+
+function updateAttributes($el /*props*/) {
+  $el.innerHTML = "";
+}
