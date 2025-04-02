58fb5235ce674ec3990653c5f719cbe42a93916f
hyunjin <71summernight@naver.com>
2025-04-03 00:35:58 +0900

    feat: createElement, eventManager 기능 작성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -11,10 +11,37 @@ export function createElement(vNode) {
   if (Array.isArray(vNode)) {
     const fragment = document.createDocumentFragment();
     vNode.flat().forEach((child) => {
-      const el = createElement(child);
-      fragment.appendChild(el);
+      fragment.appendChild(createElement(child));
     });
     return fragment;
+  } else {
+    const el = document.createElement(vNode.type);
+    const props = vNode.props || {};
+    const children = props.children;
+    if (children !== undefined && children !== null) {
+      const childArray = Array.isArray(children) ? children : [children];
+      childArray.forEach((child) => {
+        el.appendChild(createElement(child));
+      });
+    }
+    return el;
   }
 }
-// function updateAttributes($el, props) {}
+// export function updateAttributes($el, props) {
+//   for (const [key, value] of Object.entries(props)) {
+//     if (key === "children") continue;
+//     if (key === "className") {
+//       $el.setAttribute("class", value);
+//     } else {
+//       $el.setAttribute(key, value);
+//     }
+//   }
+//   const children = props.children;
+//   if (children !== undefined && children !== null) {
+//     const childArray = Array.isArray(children) ? children : [children];
+//     childArray.forEach((child) => {
+//       $el.appendChild(createElement(child));
+//     });
+//   }
+//   return $el;
+// }
