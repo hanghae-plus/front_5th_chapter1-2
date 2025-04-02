72ec07e6b36c0c6204669d23a2d0c84b82601e3c
SeongYoon <namo4862@gmail.com>
2025-04-02 15:16:15 +0900

    chore: 주저리주저리 적힌 주석제거

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,16 +1,4 @@
-/**
- * crateVNode
- * @description JSX를 vNode로 변환하는 함수
- * 어떤일을할까? JSX파일을 vNode로 변환하여 createElement를 통해 DOM으로 변환할수 있는 Node객체로 변환시켜야한다.
- * @param {*} type
- * @param {*} props
- * @param  {...any} children
- * @returns
- */
 export function createVNode(type, props, ...children) {
-  // 0401 조건부렌더링, null, undefined 처리 테스트 실패
-  // 해당부분에 대한 처리 필요.
-  // 0401 조건부렌더링, null, undefined 처리 테스트 성공, createVNode 전체 테스트 통과
   children = children.filter(
     (child) => child !== null && child !== undefined && child !== false,
   );
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,34 +1,15 @@
 import { isEmpty } from "../utils";
 
-/**
- * normalizeVNode
- * @description vNode를 정규화하는 함수
- * 왜 정규화를 해야하나? createVNode를 통해 JSX파일이 vNode로 변환되는데
- * vNode는 다양한 형태로 변환될 수 있다. 이때 vNode를 정규화하여
- * DOM으로 변환할 수 있는 형태로 만들어야 한다.
- * @param {*} vNode
- * @returns
- */
 export function normalizeVNode(vNode) {
-  // CHILD를 순회할 때 처리 될 코드들 -----
-  // 테스트코드 "null, undefined, boolean 값은 빈 문자열로 변환되어야 한다. (%s)" 에 대한 처리
   if (isEmpty(vNode)) {
     return "";
   }
-  // 테스트코드 "문자열과 숫자는 문자열로 변환되어야 한다. (%s)" 에 대한 처리
   if (typeof vNode === "string" || typeof vNode === "number") {
     return vNode.toString();
   }
   if (Array.isArray(vNode) && vNode.length === 1) {
     return vNode[0].toString();
   }
-  // --------------------------------
-  // 컴포넌트 정규화
-  // 0401 function comopnent의 props에 대한 처리 문제 발생 // {onclick, props, children}
-  // normalizeVNode({...vNode.type({ ...vNode.props, children: vNode.children }),}) 로 처리하니 문자열이 안들어감 해당처리 필요할듯.
-  // 일단은 값은 들어가는디 children에 string[]으로 들어감... => 마지막엔 문자열로 들어가야함........아닌가?
-  // falsy값은 자식에서 제거되야한다 실패 => 필터로 걸러내야할듯?
-
   if (typeof vNode.type === "function") {
     return normalizeVNode(
       vNode.type({ ...vNode.props, children: vNode.children }),
