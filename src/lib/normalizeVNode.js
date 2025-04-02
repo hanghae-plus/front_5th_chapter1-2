a55e1ba18f1e8729338322b916b5e4528d1b449a
KKys <oops9798@gmail.com>
2025-04-02 21:27:04 +0900

    normalizeVNode 정규화 제거

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -9,37 +9,9 @@ export function normalizeVNode(vNode) {
     return String(vNode);
   }
 
-  // 배열 처리
-  if (Array.isArray(vNode)) {
-    console.log(vNode);
-    return vNode.map((child) => normalizeVNode(child)).flat();
-  }
-
-  // 함수형 컴포넌트 처리
-  if (typeof vNode.type === "function") {
-    const Component = vNode.type;
-    return normalizeVNode(Component(vNode.props || {}));
-  }
-
-  // VNode 객체 처리
-  const normalizedChildren = [];
-  if (vNode.children) {
-    const children = Array.isArray(vNode.children)
-      ? vNode.children
-      : [vNode.children];
-    children.forEach((child) => {
-      const normalized = normalizeVNode(child);
-      if (Array.isArray(normalized)) {
-        normalizedChildren.push(...normalized);
-      } else if (normalized !== "") {
-        normalizedChildren.push(normalized);
-      }
-    });
-  }
-
   return {
     type: vNode.type,
     props: vNode.props || {},
-    children: normalizedChildren,
+    children: vNode.children,
   };
 }
