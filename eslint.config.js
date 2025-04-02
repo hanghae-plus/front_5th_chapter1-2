3b51d9887393b9f40ab454ec912c64cea90bd0f7
hanbeulYou <hanbeul.you@gmail.com>
2025-04-02 16:34:45 +0900

    chore: eslint config 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,38 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import process from "node:process";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Config[]} */
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: true,
+        tsconfigRootDir: process.cwd(),
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": pluginTs,
+    },
+    rules: {
+      ...pluginTs.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
   pluginJs.configs.recommended,
   eslintPluginPrettier,
   eslintConfigPrettier,
