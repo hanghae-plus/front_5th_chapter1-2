6e46f1e2db31831d7e819475df79fad9a9f3b19b
SeongYoon <namo4862@gmail.com>
2025-04-02 21:13:45 +0900

    fix: UpdateElement 추가 및 기존 노드를 저장하기위한 객체 설정

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,11 +1,18 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
+
+const oldNodes = { node: null };
 
 export function renderElement(vNode, container) {
   const normalizedVNode = normalizeVNode(vNode);
-  const getCreateElement = createElement(normalizedVNode);
-  container.appendChild(getCreateElement);
+  if (container.childNodes.length) {
+    updateElement(container, normalizedVNode, oldNodes["node"]);
+  } else {
+    const getCreateElement = createElement(normalizedVNode);
+    container.appendChild(getCreateElement);
+    oldNodes.node = normalizedVNode;
+  }
   setupEventListeners(container);
 }
