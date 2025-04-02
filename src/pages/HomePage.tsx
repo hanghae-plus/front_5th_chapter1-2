4c07b79ea0917ceb9534a077301b93a014b2cad2
hanbeulYou <hanbeul.you@gmail.com>
2025-04-03 02:16:19 +0900

    fix: Home 페이지 tsx 전환

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,12 +4,6 @@ import { createVNode } from "../lib/vdom";
 import { Footer, Header, Navigation, Post, PostForm } from "../components";
 import { globalStore } from "../stores";
 
-/**
- * 심화과제
- * - 로그인한 사용자는 게시물을 추가할 수 있다.
- * - 로그인한 사용자는 게시물에 좋아요를 누를 수 있다.
- * - 로그인하지 않은 사용자가 게시물에 좋아요를 누를 경우, "로그인 후 이용해주세요"를 alert로 띄운다.
- */
 export const HomePage = () => {
   const { posts, loggedIn } = globalStore.getState();
 
