951fb61bc4014f131bc5f8e3da79ee17f21f8663
User850413 <csyou1228@gmail.com>
2025-04-02 20:49:44 +0900

    04/02 :sparkles: : eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,20 @@
-export function setupEventListeners(root) {}
+export function setupEventListeners(root) {
+  const els = document.querySelectorAll("*");
+  els.forEach((el) =>
+    root.addEventListener(el["custom-event"], (e) => {
+      if (e.target === el) {
+        el["custom-handler"]();
+      }
+    }),
+  );
+}
 
-export function addEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  element["custom-event"] = eventType;
+  element["custom-handler"] = handler;
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function removeEvent(element, eventType, handler) {
+  if (element["custom-event"] === eventType) element["custom-event"] = null;
+  if (element["custom-handler"] === handler) element["custom-handler"] = null;
+}
