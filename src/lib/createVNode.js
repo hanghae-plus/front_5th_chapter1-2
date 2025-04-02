14100519cc663727a097e0515c709a9d59e5e2e2
JiHwanLee <kdgosu53@gmail.com>
2025-04-02 22:04:02 +0900

    createVNode 개발

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,13 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  // console.log("createVNodeß");
+
+  console.log(`createVNode > children: ${children}`);
+  // console.log(children);
+  return {
+    type,
+    props,
+    children: children
+      .flat(2)
+      .filter((v) => v !== null && v !== undefined && v !== false),
+  };
 }
