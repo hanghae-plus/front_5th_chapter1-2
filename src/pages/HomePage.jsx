b2d33e62e02113e9463d3c078cc5185e5fa36b6b
이흥수 [Frontend] <heungsoo1@gmail.com>
2025-04-02 16:32:05 +0900

    feat: post submit, like 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,14 +4,73 @@ import { createVNode } from "../lib";
 import { Footer, Header, Navigation, Post, PostForm } from "../components";
 import { globalStore } from "../stores";
 
-/**
- * 심화과제
- * - 로그인한 사용자는 게시물을 추가할 수 있다.
- * - 로그인한 사용자는 게시물에 좋아요를 누를 수 있다.
- * - 로그인하지 않은 사용자가 게시물에 좋아요를 누를 경우, "로그인 후 이용해주세요"를 alert로 띄운다.
- */
 export const HomePage = () => {
-  const { posts } = globalStore.getState();
+  const { currentUser, posts } = globalStore.getState();
+  const { loggedIn } = globalStore.getState();
+
+  const handlePostLike = (postId) => {
+    if (!loggedIn) {
+      alert("로그인 후 이용해주세요");
+
+      return;
+    }
+
+    const targetPost = posts.find((post) => post.id === postId);
+    const isAlreadyLiked = targetPost.likeUsers.includes(currentUser.username);
+
+    if (isAlreadyLiked) {
+      targetPost.likeUsers = targetPost.likeUsers.filter(
+        (user) => user !== currentUser.username,
+      );
+    } else {
+      targetPost.likeUsers.push(currentUser.username);
+    }
+
+    globalStore.setState({
+      posts: posts.map((post) => (post.id === postId ? targetPost : post)),
+    });
+  };
+
+  const renderPost = () => {
+    const isLiked = (postId) => {
+      if (!currentUser) return false;
+
+      const targetPost = posts.find((post) => post.id === postId);
+
+      return targetPost.likeUsers.includes(currentUser.username);
+    };
+
+    return (
+      <div id="posts-container" className="space-y-4">
+        {[...posts]
+          .sort((a, b) => b.time - a.time)
+          .map((props) => (
+            <Post
+              {...props}
+              activationLike={isLiked(props.id)}
+              onPostLike={() => handlePostLike(props.id)}
+            />
+          ))}
+      </div>
+    );
+  };
+
+  const handlePostSubmit = () => {
+    const postContent = document.getElementById("post-content").value;
+
+    globalStore.setState({
+      posts: [
+        ...posts,
+        {
+          id: posts.length + 1,
+          author: currentUser.username,
+          time: Date.now(),
+          content: postContent,
+          likeUsers: [],
+        },
+      ],
+    });
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen flex justify-center">
@@ -20,14 +79,8 @@ export const HomePage = () => {
         <Navigation />
 
         <main className="p-4">
-          <PostForm />
-          <div id="posts-container" className="space-y-4">
-            {[...posts]
-              .sort((a, b) => b.time - a.time)
-              .map((props) => {
-                return <Post {...props} activationLike={false} />;
-              })}
-          </div>
+          {loggedIn && <PostForm onPostSubmit={handlePostSubmit} />}
+          {renderPost()}
         </main>
 
         <Footer />
