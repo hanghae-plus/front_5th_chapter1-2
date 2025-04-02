0f907aaf878e0c2e8cde03fdb71f34dc1358b618
sungchan.kim <sungchan.kim@datastreams.co.kr>
2025-04-02 10:08:01 +0900

    Fix: eventManager, renderElement, updateElement

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,16 +1,37 @@
-const eventsMap = new Map();
+const handlers = new Map();
+// handlers = {
+//   element: {
+//      click: (e) => {...}
+//   }
+// }
+const eventTypes = new Set();
+// eventTypes = ['click', 'mousedown', ...]
+
+const handleEvents = (e) => {
+  const handler = handlers.get(e.target)?.get(e.type);
+  handler?.(e);
+};
 
 export function setupEventListeners(root) {
-  eventsMap.set("root", root);
-  eventsMap.forEach(({ eventType, handler }) => {
-    root.addEventListener(eventType, handler);
+  eventTypes.forEach((eventType) => {
+    root.addEventListener(eventType, handleEvents);
   });
 }
 
 export function addEvent(element, eventType, handler) {
-  eventsMap.set(element, { eventType, handler });
+  eventTypes.add(eventType);
+
+  if (!handlers.has(element)) {
+    handlers.set(element, new Map());
+  }
+  const elementHandlers = handlers.get(element);
+  elementHandlers.set(eventType, handler);
 }
 
 export function removeEvent(element, eventType, handler) {
-  (eventsMap.get("root") || element).removeEventListener(eventType, handler);
+  const elementHandlers = handlers.get(element);
+  if (elementHandlers?.has(eventType)) {
+    elementHandlers.delete(eventType);
+  }
+  console.log(handler);
 }
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,65 @@
 import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+function updateAttributes($target, originNewProps, originOldProps) {
+  if (originOldProps !== null && typeof originOldProps !== "undefined") {
+    Object.entries(originOldProps).forEach(([key, value]) => {
+      if (key.startsWith("on") && typeof value === "function") {
+        removeEvent($target, key.replace("on", "").toLowerCase(), value);
+      } else {
+        $target.removeAttribute(key);
+      }
+    });
+  }
+  if (originNewProps !== null && typeof originNewProps !== "undefined") {
+    Object.entries(originNewProps).forEach(([key, value]) => {
+      if (key === "className") key = "class";
+      if (key.startsWith("on") && typeof value === "function") {
+        addEvent($target, key.replace("on", "").toLowerCase(), value);
+      } else {
+        $target.setAttribute(key, value);
+      }
+    });
+  }
+}
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  const $oldEl = parentElement.childNodes[index];
+  // newNode vs oldNode
+  if (newNode && !oldNode) {
+    parentElement.append(createElement(newNode));
+    return;
+  }
+  if (!newNode && oldNode) {
+    parentElement.removeChild($oldEl);
+    return;
+  }
+  // node가 string일 경우
+  if (typeof newNode === "string") {
+    if (newNode !== oldNode) {
+      parentElement.textContent = newNode;
+      return;
+    }
+  }
+
+  // 같은 타입이면 props 업데이트
+  updateAttributes($oldEl, newNode.props, oldNode.props);
+
+  // node가 다른 타입으로 교체된 경우
+  if (newNode.type !== oldNode.type) {
+    parentElement.removeChild($oldEl);
+    parentElement.append(createElement(newNode));
+    return;
+  }
+
+  // node가 children을 가진 경우
+  const longerOne = Math.max(
+    newNode?.children?.length || 0,
+    oldNode?.children?.length || 0,
+  );
+  if (longerOne > 0) {
+    for (let i = 0; i < longerOne; i++) {
+      updateElement($oldEl, newNode?.children[i], oldNode?.children[i], i);
+    }
+  }
+}
