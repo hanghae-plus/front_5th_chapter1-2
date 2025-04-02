de6a151e2084952cad8e23ef45e1766672fdf543
SeongYoon <namo4862@gmail.com>
2025-04-02 16:27:02 +0900

    fix: eventManager 수정
    basic 테스트의 마지막인 이벤트핸들러는 update기능이 추가되어야 가능할것으로 판단.

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,36 @@
-export function setupEventListeners(root) {}
+const globalEventHandlers = {};
 
-export function addEvent(element, eventType, handler) {}
+const handleGlobalEvents = (e) => {
+  const thisEventObject = globalEventHandlers[e.type];
+  for (const tagName in thisEventObject) {
+    if (e.target.tagName === tagName) {
+      thisEventObject[tagName](e);
+    }
+  }
+};
 
-export function removeEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  Object.keys(globalEventHandlers).forEach((eventType) => {
+    root.addEventListener(eventType, handleGlobalEvents);
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!globalEventHandlers[eventType]) {
+    globalEventHandlers[eventType] = {};
+  }
+  globalEventHandlers[eventType][element.tagName] = handler;
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (!globalEventHandlers[eventType]) return;
+
+  const eventHandlers = globalEventHandlers[eventType];
+
+  if (
+    eventHandlers[element.tagName] &&
+    eventHandlers[element.tagName] === handler
+  ) {
+    delete eventHandlers[element.tagName];
+  }
+}
