db04e033389b4d000832dac59f915f652b3fbf28
choiseoeun-GoUp <seoeun0321@gmail.com>
2025-04-02 23:32:19 +0900

    feat : post 기능 추가

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,10 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../stores";
 
 export const PostForm = () => {
+  const { post } = globalStore.actions;
+
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -12,6 +15,11 @@ export const PostForm = () => {
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        onClick={(e) => {
+          e.preventDefault();
+          const content = document.querySelector("#post-content").value;
+          post(content);
+        }}
       >
         게시
       </button>
