9141ccc089354ddd43a4765f4c6b7553c07c4dac
JiHwanLee <kdgosu53@gmail.com>
2025-04-03 22:30:50 +0900

    배포 환경 설정

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,26 @@
 import { createObserver } from "./createObserver";
 
+const PROD_URL = "/front_5th_chapter1-2";
+
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const getPath = () => {
+    const baseUrl = import.meta.env.MODE === "production" ? PROD_URL : "";
+    return window.location.pathname.replace(baseUrl, "") || "/";
+  };
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
-    window.history.pushState(null, null, path);
+    if (import.meta.env.MODE === "development") {
+      window.history.pushState(null, null, path);
+    } else {
+      const baseUrl = import.meta.env.MODE === "production" ? PROD_URL : "";
+      const fullPath = baseUrl + path;
+
+      window.history.pushState(null, null, fullPath);
+    }
     notify();
   };
 
