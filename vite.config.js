5c098a6a0379b5de1fab3d677d99c21203efe12c
EZONE <meowoof01@gmail.com>
2025-04-03 17:05:26 +0900

    chore 배포 test

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,38 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
-export default mergeConfig(
-  defineConfig({
-    base: "./",
-    esbuild: {
-      jsxFactory: "createVNode",
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        jsx: "transform",
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineConfig({
+      esbuild: {
         jsxFactory: "createVNode",
       },
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-          hash: resolve(__dirname, "index.hash.html"),
-          notFound: resolve(__dirname, "404.html"),
+      optimizeDeps: {
+        esbuildOptions: {
+          jsx: "transform",
+          jsxFactory: "createVNode",
         },
       },
-    },
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.js",
-      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    },
-  }),
-);
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.js",
+        exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+      },
+      base: env.VITE_BASE_URL,
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            hash: resolve(__dirname, "index.hash.html"),
+          },
+        },
+      },
+    }),
+  );
+};
