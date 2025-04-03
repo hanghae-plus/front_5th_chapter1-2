58c775a98d03a6258679a3fb9350860d82019f60
3jun <kk3june@gmail.com>
2025-04-03 23:10:46 +0900

    feat. post 기능 구현

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,14 +1,45 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 import { toTimeFormat } from "../../utils/index.js";
 
 export const Post = ({
+  id,
   author,
   time,
   content,
   likeUsers,
   activationLike = false,
 }) => {
+  const { currentUser, loggedIn, posts } = globalStore.getState();
+
+  const onLikeToggle = () => {
+    if (!loggedIn) {
+      return alert("로그인 후 이용해주세요");
+    } else {
+      const newPosts = [...posts].map((post) => {
+        if (post.id === id) {
+          if (post.likeUsers.includes(currentUser.username)) {
+            return {
+              ...post,
+              likeUsers: likeUsers.filter((it) => it !== currentUser.username),
+            };
+          } else {
+            return {
+              ...post,
+              likeUsers: [...likeUsers, currentUser.username],
+            };
+          }
+        }
+        return post;
+      });
+
+      console.log(newPosts);
+      globalStore.setState({
+        posts: newPosts,
+      });
+    }
+  };
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
       <div className="flex items-center mb-2">
@@ -21,6 +52,7 @@ export const Post = ({
       <div className="mt-2 flex justify-between text-gray-500">
         <span
           className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
+          onClick={onLikeToggle}
         >
           좋아요 {likeUsers.length}
         </span>
--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,27 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 
 export const PostForm = () => {
+  const { currentUser, posts } = globalStore.getState();
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const content = document.getElementById("post-content").value;
+
+    globalStore.setState({
+      posts: [
+        ...posts,
+        {
+          id: posts.length + 1,
+          author: currentUser?.username,
+          time: new Date(),
+          content: content,
+          likeUsers: [],
+        },
+      ],
+    });
+  };
+
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -12,6 +32,7 @@ export const PostForm = () => {
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        onClick={onSubmit}
       >
         게시
       </button>
