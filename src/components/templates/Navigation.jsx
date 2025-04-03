da9546eddf1594c80adfe18581c32d8a21c18235
maphnew <zcm3118@gmail.com>
2025-04-03 20:38:41 +0900

    Chore: 배포 관련 수정

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -2,10 +2,13 @@
 import { createVNode } from "../../lib";
 import { router } from "../../router";
 import { globalStore } from "../../stores";
+import { BASE_ROUTE } from "../../constants/basePath";
 
 const getNavItemClass = (path) => {
   const currentPath = window.location.pathname;
-  return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
+  return currentPath === BASE_ROUTE + path
+    ? "text-blue-600 font-bold"
+    : "text-gray-600";
 };
 
 function Link({ onClick, children, ...props }) {
---
+++ src/constants/basePath.js
@@ -0,0 +1,2 @@
+export const BASE_ROUTE =
+  import.meta.env.MODE === "production" ? "/front_5th_chapter1-2" : "";
