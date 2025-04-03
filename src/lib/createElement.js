6588b15ad03b6498c31d1e5a11b92ceed7bbd73a
devchaeyoung <chaemaa00@gmail.com>
2025-04-03 13:55:47 +0900

    feat: createVNode추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,25 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  const v = typeof vNode;
+  if (v === "boolean" || v === "undefined" || vNode === null) {
+    return document.createTextNode("");
+  }
+  if (v === "string" || v === "number") {
+    return document.createTextNode(vNode);
+  }
 
-function updateAttributes($el, props) {}
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    fragment.append(...vNode.map(createElement));
+    return fragment;
+  }
+  const el = document.createElement(vNode.type);
+  updateAttributes(el, vNode.props);
+
+  return el;
+}
+
+function updateAttributes($el, props) {
+  return { $el, props }; // 함수일때에 반환하는 코드를 작성.  각각 테스트 코드에 맞는 코드를 작성하면됨.
+}
--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,8 @@
+/* 배열을 평탄화 시키는 이유? */
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children.filter((v) => v === 0 || Boolean(v)).flat(Infinity),
+  };
 }
