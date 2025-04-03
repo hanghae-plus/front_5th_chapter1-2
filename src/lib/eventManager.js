f673dd42daa1bdd1e902bfee3d7f6321de129006
User850413 <csyou1228@gmail.com>
2025-04-03 18:06:45 +0900

    04/03 :sparkles: : eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,20 +1,40 @@
+const events = new Map();
+let isListening = false;
+
 export function setupEventListeners(root) {
-  const els = document.querySelectorAll("*");
-  els.forEach((el) =>
-    root.addEventListener(el["custom-event"], (e) => {
-      if (e.target === el) {
-        el["custom-handler"]();
-      }
-    }),
-  );
+  if (isListening) return;
+
+  function getEventHandlers(e) {
+    events.forEach((event) => {
+      if (event.has(e.target)) event.get(e.target).forEach((fn) => fn(e));
+    });
+  }
+
+  events.forEach((_, eventType) => {
+    root.addEventListener(eventType, getEventHandlers);
+  });
+
+  isListening = true;
 }
 
 export function addEvent(element, eventType, handler) {
-  element["custom-event"] = eventType;
-  element["custom-handler"] = handler;
+  if (!events.has(eventType)) events.set(eventType, new Map());
+  const eventEl = events.get(eventType);
+
+  const handlers = new Set();
+  handlers.add(handler);
+
+  if (!eventEl.has(element)) {
+    eventEl.set(element, handlers);
+  } else {
+    eventEl.get(element).add(handler);
+  }
 }
 
 export function removeEvent(element, eventType, handler) {
-  if (element["custom-event"] === eventType) element["custom-event"] = null;
-  if (element["custom-handler"] === handler) element["custom-handler"] = null;
+  const eventEls = events.get(eventType);
+
+  if (!eventEls || !eventEls.has(element)) return;
+  const handlers = events.get(eventType).get(element);
+  handlers.delete(handler);
 }
