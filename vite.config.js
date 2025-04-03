d7696b75ed330fdc18173087ff80f9562e361fed
JiYu <eunjun1018@gmail.com>
2025-04-04 03:04:32 +0900

    index.html 수정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default mergeConfig(
   defineConfig({
-    base: "/",
+    base: "/hanghae99-chap2/",
     esbuild: {
       jsxFactory: "createVNode",
       loader: "jsx",
