0e1e6cc2e346efb272ca8ed22540442833bf078b
ParkYongHo1 <qkaxhf8823@naver.com>
2025-04-03 09:04:37 +0900

    Feat | enevtManager, rednderElement, updateElement 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,43 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+const eventTypeArr = [];
 
-export function removeEvent(element, eventType, handler) {}
+export function setupEventListeners($root) {
+  eventTypeArr.forEach((eventType) => {
+    $root.addEventListener(eventType, handleDelegatedEvent);
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventTypeArr.includes(eventType)) {
+    eventTypeArr.push(eventType);
+  }
+
+  const elementEvents = eventMap.get(element) || [];
+  elementEvents.push({ eventType, handler });
+
+  eventMap.set(element, elementEvents);
+}
+export function removeEvent(element, eventType, handler) {
+  if (eventMap.has(element)) {
+    const elementEvents = eventMap.get(element);
+    const updatedElementEvents = elementEvents.filter(
+      (h) => !(h.handler === handler && h.eventType === eventType),
+    );
+    eventMap.set(element, updatedElementEvents);
+    if (updatedElementEvents.length === 0) {
+      eventMap.delete(element);
+    }
+  }
+}
+
+function handleDelegatedEvent(e) {
+  if (!eventMap.has(e.target)) return;
+
+  const elementEvents = eventMap.get(e.target);
+  const filtered = elementEvents.filter(
+    ({ eventType }) => eventType === e.type,
+  );
+
+  filtered.forEach(({ handler }) => handler(e));
+}
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,57 @@
 import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  const childNode = parentElement.childNodes[index];
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  if (!newNode && oldNode) {
+    parentElement.removeChild(childNode);
+    return;
+  }
+
+  if (newNode && !oldNode) {
+    parentElement.appendChild(createElement(newNode));
+    return;
+  }
+
+  if (typeof newNode === "string" && typeof oldNode === "string") {
+    if (newNode === oldNode) return;
+    parentElement.replaceChild(createElement(newNode), childNode);
+    return;
+  }
+
+  if (newNode.type !== oldNode.type) {
+    parentElement.replaceChild(createElement(newNode), childNode);
+    return;
+  }
+
+  updateAttributes(childNode, newNode.props || {}, oldNode.props || {});
+
+  const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+  for (let i = 0; i < maxLength; i++) {
+    updateElement(
+      parentElement.childNodes[index],
+      newNode.children[i],
+      oldNode.children[i],
+      i,
+    );
+  }
+}
+
+function updateAttributes(target, newProps, oldProps) {
+  Object.keys(oldProps).forEach((key) => {
+    if (key.startsWith("on") && typeof oldProps[key] === "function") {
+      removeEvent(target, key.substring(2).toLowerCase(), oldProps[key]);
+    }
+  });
+
+  Object.entries(newProps).forEach(([key, value]) => {
+    if (key === "className") {
+      target.setAttribute("class", value);
+    } else if (key.startsWith("on") && typeof value === "function") {
+      addEvent(target, key.slice(2).toLowerCase(), value);
+    } else {
+      target.setAttribute(key, value);
+    }
+  });
+}
