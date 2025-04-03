ed30e6e4c97047614c72efce21c0f8085adfe2fd
배규덕 <mandarinduk@gmail.com>
2025-04-04 02:03:14 +0900

    feat: 심화 과제

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,21 +3,22 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
-let initialVNode = null;
+const containerVNodeMap = new WeakMap();
 
 export function renderElement(vNode, container) {
   const normalizedVNode = normalizeVNode(vNode);
 
+  const prevVNode = containerVNodeMap.get(container);
+
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  if (!initialVNode) {
-    initialVNode = normalizedVNode;
+  if (!prevVNode) {
     const newElement = createElement(normalizedVNode);
     container.appendChild(newElement);
+    setupEventListeners(container);
   } else {
     // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-    updateElement(container, normalizedVNode, initialVNode);
+    updateElement(container, normalizedVNode, prevVNode);
   }
 
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
-  setupEventListeners(container);
+  containerVNodeMap.set(container, normalizedVNode);
 }
