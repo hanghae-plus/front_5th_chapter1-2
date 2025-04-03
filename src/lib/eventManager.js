8652da03dd35fb5bdcd6f8d1aa5d745fdad9f706
Heejin <dev.heejiin@gmail.com>
2025-04-03 21:23:58 +0900

    feat: eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,36 @@
-export function setupEventListeners(root) {}
+const eventStore = new Map();
+const eventType = ["click", "input", "change", "keydown", "keyup"];
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventType.forEach((eventType) =>
+    root.addEventListener(eventType, dispatchEventHandler),
+  );
+}
 
-export function removeEvent(element, eventType, handler) {}
+function dispatchEventHandler(event) {
+  let target = event.target;
+
+  while (target && target !== event.currentTarget) {
+    const handlers = eventStore.get(target);
+    if (handlers?.[event.type]) {
+      handlers[event.type](event);
+      return;
+    }
+    target = target.parentElement;
+  }
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventStore.has(element)) {
+    eventStore.set(element, {});
+  }
+  eventStore.get(element)[eventType] = handler;
+}
+
+export function removeEvent(element, eventType) {
+  const handlers = eventStore.get(element);
+  if (handlers) {
+    delete handlers[eventType];
+    if (Object.keys(handlers).length === 0) eventStore.delete(element);
+  }
+}
