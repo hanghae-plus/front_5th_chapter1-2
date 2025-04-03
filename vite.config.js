ce6daaa1414386665e1cdf0c9d51b67fe4ff9320
SoominYim <soo15soo@kakao.com>
2025-04-03 16:31:03 +0900

    chore : 배포 build 스크립트 및 Vite 설정 개선 - package.json에 배포 스크립트 추가 - Vite 설정에 빌드 옵션 및 엔트리 파일 경로 및 배포 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default mergeConfig(
   defineConfig({
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-2/" : "/",
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"), // 메인 엔트리 파일
+          hash: resolve(__dirname, "index.hash.html"), // 해시 엔트리 파일
+          404: resolve(__dirname, "404.html"), // 404 페이지 포함
+        },
+      },
+    },
     esbuild: {
       jsxFactory: "createVNode",
     },
