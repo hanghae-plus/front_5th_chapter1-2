f2fc4de3c706a1f2be45f44a385afaa842a1b5e7
Wonny <h.wonny11@gmail.com>
2025-04-04 03:35:09 +0900

    fix: renderElement function

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,36 +3,30 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
+// 이전 가상 노드를 저장할 WeakMap
+const previousNodeMap = new WeakMap();
+
 export function renderElement(vNode, container) {
   // 1. vNode를 정규화
-  const _normalizeVNode = normalizeVNode(vNode);
+  const newNode = normalizeVNode(vNode);
 
   // 2. container의 첫번째 자식 요소 확인
-  const oldElement = container.firstElementChild;
+  // const oldElement = container.firstElementChild;
+  const oldElement = previousNodeMap.get(container);
 
-  // 3. 최초 렌더링시에는 createElement로 DOM을 생성
+  // 3. 최초 렌더링 또는 이전 노드가 없는 경우
   if (!oldElement) {
-    const element = createElement(_normalizeVNode);
-
-    // 요소에 현재 vNode 정보 저장 (나중에 비교를 위해)
-    element.__vNode = _normalizeVNode;
-
-    // container에 요소 추가
+    const element = createElement(newNode);
     container.appendChild(element);
   } else {
-    // updateElement로 기존 DOM을 업데이트
-    const updatedElement = updateElement(
+    updateElement(
       container, // 부모 요소
-      _normalizeVNode, // 새로운 노드
-      oldElement.__vNode, // 이전 노드 (container의 첫 번째 자식의 이전 가상 노드)
-      0, // 첫 번째 자식 요소이므로 index는 0
+      newNode, // 새로운 노드
+      oldElement, // 이전 노드 (container의 첫 번째 자식의 이전 가상 노드)
     );
-
-    if (updatedElement) {
-      // 업데이트된 요소에 새 vNode 정보 저장
-      updatedElement.__vNode = _normalizeVNode;
-    }
   }
-  // 4. container에 이벤트를 등록
+  // 4. 현재 가상 노드를 previousNodeMap에 저장
+  previousNodeMap.set(container, newNode);
+  // 5. container에 이벤트를 등록
   setupEventListeners(container);
 }
