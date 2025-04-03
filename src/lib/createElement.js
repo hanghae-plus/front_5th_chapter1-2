c18527a62d69474230dae3ff9b898d374b96eca6
devchaeyoung <chaemaa00@gmail.com>
2025-04-04 02:10:31 +0900

    feat: createElement 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,25 +1,49 @@
-// import { addEvent } from "./eventManager";
+import { addEvent } from "./eventManager";
 
 export function createElement(vNode) {
-  const v = typeof vNode;
-  if (v === "boolean" || v === "undefined" || vNode === null) {
+  if (
+    typeof vNode === "boolean" ||
+    typeof vNode === "undefined" ||
+    vNode === null
+  ) {
     return document.createTextNode("");
   }
-  if (v === "string" || v === "number") {
+  if (typeof vNode === "string" || typeof vNode === "number") {
     return document.createTextNode(vNode);
+  }
+
+  const $el = document.createElement(vNode.type);
+  if (vNode.props) {
+    updateAttributes($el, vNode.props);
   }
 
   if (Array.isArray(vNode)) {
     const fragment = document.createDocumentFragment();
-    fragment.append(...vNode.map(createElement));
+    vNode.forEach((v) => {
+      fragment.appendChild(createElement(v));
+    });
     return fragment;
   }
-  const el = document.createElement(vNode.type);
-  updateAttributes(el, vNode.props);
 
-  return el;
+  const children = vNode.children || [];
+  children.forEach((v) => $el.appendChild(createElement(v)));
+
+  return $el;
 }
 
 function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (typeof value === "function") {
+      addEvent($el, key, value);
+      // export function addEvent(element, eventType, handler) {}
+    }
+
+    if (key === "id") {
+      $el.setAttribute(key, value);
+    }
+    if (key === "className") {
+      $el.setAttribute("class", value);
+    }
+  });
   return { $el, props }; // 함수일때에 반환하는 코드를 작성.  각각 테스트 코드에 맞는 코드를 작성하면됨.
 }
