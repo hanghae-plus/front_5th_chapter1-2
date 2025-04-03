9046950a60f2c8c6241a58d6c838fa63d5949acd
devchaeyoung <chaemaa00@gmail.com>
2025-04-04 01:11:10 +0900

    feat: normalizeVNode 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,24 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    const component = vNode.type;
+    const props = vNode.props;
+    return normalizeVNode(
+      component({ ...props, children: vNode.children || [] }),
+    );
+  }
+  if (Array.isArray(vNode.children)) {
+    const children = normalizeVNode(vNode.children)
+      .map((v) => normalizeVNode(v))
+      .filter((v) => v !== "");
+    return { ...vNode, children: children };
+  }
   return vNode;
 }
